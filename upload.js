5a2291b69ae21eced00e93e13bdb5299195d514b
Kriston Burnstein <kriston.burnstein@gmail.com>
2023-07-24 10:56:54 -0400

    Cloudinary test 5: optimize all 3 get-started image uploads and retain quality by pre-transforming image to 2000px width and webp format

--- upload.js
+++ upload.js
@@ -1,16 +1,14 @@
-var cloudinary = require('./middleware/cloudinary');
+let cloudinary = require('./middleware/cloudinary');
 
 cloudinary.uploader.upload("./public/imgs/sequoia2.jpg", 
     { width: 2000, format: "webp" },
     function(error, result) { console.log(result, error) });
 
+cloudinary.uploader.upload("./public/imgs/pier.jpg", 
+    { width: 2000, format: "webp" },
+    function(error, result) { console.log(result, error) });
 
-// reimplement and test after cloudinary cookies issues resolved 
-    // cloudinary.uploader.upload("./public/imgs/pier.jpg", 
-    // { width: 2000, format: "webp" },
-    // function(error, result) { console.log(result, error) });
-
-    // cloudinary.uploader.upload("./public/imgs/tree.jpg", 
-    // { width: 2000, format: "webp" },
-    // function(error, result) { console.log(result, error) });
+cloudinary.uploader.upload("./public/imgs/tree.jpg", 
+    { width: 2000, format: "webp" },
+    function(error, result) { console.log(result, error) });
 
